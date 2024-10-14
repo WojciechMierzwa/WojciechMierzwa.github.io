dcee883707aa8668da2ed5a9affc64378e9dc561
WojciechMierzwa <wojciech.piotr.mierzwa@gmail.com>
2024-10-14 11:11:25 +0200

    Zad4
    Moduł string-width umożliwia mierzenie szerokości tekstu, co jest ważne w aplikacjach działających w terminalu, gdzie niektóre znaki (np. z języków azjatyckich, emoji) mogą zajmować podwójną szerokość. Dodatkowo, kody ANSI do formatowania tekstu są ignorowane w obliczeniach, co pozwala na uzyskanie dokładnych pomiarów tylko dla widocznych znaków.

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Języki Wysokiego Poziomu w Aplikacjach Internetowych
 
-Lab2: [Mierzwa Wojciech Lab JS](https://wojciechmierzwa.github.io/MierzwaWojciechLabJS/)
+Lab3: [Mierzwa Wojciech Lab JS](https://github.com/WojciechMierzwa/WojciechMierzwa.github.io)
 
 ### Autor
 **Wojciech Mierzwa**  
 
 ### Indeks
 20858
+
--- index.js
+++ index.js
@@ -1,43 +1,13 @@
-const http = require('http');
-const path = require("path");
-const util = require("util");
-const v8 = require("v8");
-const hostname = '127.0.0.1';
-const port = 8888;
-const server = http.createServer((request, response) => {
-response.statusCode = 200;
-response.setHeader('Content-Type', 'text/plain');
-response.end('Hello, from node.js serwer !\n');
-});
-server.listen(port, hostname, () => {
-util.log(v8.getHeapStatistics());
-console.log(path.basename(__filename));
-util.log(path.join(__dirname, 'uploads','images'));
-console.log(`Server running at http://${hostname}:${port}/`);
-});
+import stringWidth from 'string-width';
 
-/*Po podmienieniu zawartości pliku dostaliśmy poniższy output. Wnioskiem jest, że nodemon jest przydatny gdy na bieżąco chcemy analizować zmiany 
-które zaszły po zmianie kodu bo nie trzeba ręcznie restartować servera, bo przy każdej zmianie automatycznie jest restartowany
+// Mierzenie szerokości prostego ciągu znaków
+const name = 'Wojciech Mierzwa';
+console.log(`Długość mojego imienia "${name}" ma: ${stringWidth(name)} bajtów.`);  
 
-Server running at http://127.0.0.1:8888/
-[nodemon] restarting due to changes...
-[nodemon] starting `node index.js`
-14 Oct 10:49:27 - {
-  total_heap_size: 4927488,
-  total_heap_size_executable: 262144,
-  total_physical_size: 4927488,      
-  total_available_size: 2194292408,  
-  used_heap_size: 4453104,
-  heap_size_limit: 2197815296,       
-  malloced_memory: 262264,
-  peak_malloced_memory: 206880,      
-  does_zap_garbage: 0,
-  number_of_native_contexts: 2,      
-  number_of_detached_contexts: 0,    
-  total_global_handles_size: 8192,   
-  used_global_handles_size: 2560,    
-  external_memory: 1737097
-}
-index.js
-14 Oct 10:49:27 - E:\Semestr5\Języki wysokiego poziomu w aplikacjach internetowych\WojciechMierzwa.github.io\uploads\images
-Server running at http://127.0.0.1:8888/ */
+// Mierzenie szerokości znaku wielobajtowego
+const multiByteChar = '😃';
+console.log(`Emoji "${multiByteChar}" ma: ${stringWidth(multiByteChar)} bajty.`);  
+
+// Mierzenie szerokości znaku z kodami ucieczki ANSI
+const formattedChar = '\u001B[1m✂️ \u001B[22m';
+console.log(` "${formattedChar}" ma: ${stringWidth(formattedChar)} bajty (liczone są tylko widoczne znaki).`);  
