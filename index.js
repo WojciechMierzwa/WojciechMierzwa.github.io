134337e9753d37d781de356ee91eec92b72fb71e
WojciechMierzwa <wojciech.piotr.mierzwa@gmail.com>
2024-10-14 10:32:25 +0200

    Lab3 Zad1
    //WNIOSEK
    
    /* Node.js działa asynchronicznie czyli nie czeka na
    zakończenie długotrwałych operacji*/

---
+++ index.js
@@ -0,0 +1,11 @@
+const wait = 3000;
+console.log("Początek skryptu...");
+setTimeout(function(){
+console.log("Upłynęły minimum 3 sek...")
+}, wait);
+console.log("koniec skryptu...");
+
+//WNIOSEK
+
+/* Node.js działa asynchronicznie czyli nie czeka na
+zakończenie długotrwałych operacji*/
