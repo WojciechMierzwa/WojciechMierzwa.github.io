c21008238ad61d41b35a95c592b60c0c46974900
WojciechMierzwa <wojciech.piotr.mierzwa@gmail.com>
2024-10-14 10:53:29 +0200

    Lab3Zad3
    /*Po podmienieniu zawartości pliku dostaliśmy poniższy output. Wnioskiem jest, że nodemon jest przydatny gdy na bieżąco chcemy analizować zmiany
    które zaszły po zmianie kodu bo nie trzeba ręcznie restartować servera, bo przy każdej zmianie automatycznie jest restartowany
    
    Server running at http://127.0.0.1:8888/
    [nodemon] restarting due to changes...
    [nodemon] starting `node index.js`
    14 Oct 10:49:27 - {
      total_heap_size: 4927488,
      total_heap_size_executable: 262144,
      total_physical_size: 4927488,
      total_available_size: 2194292408,
      used_heap_size: 4453104,
      heap_size_limit: 2197815296,
      malloced_memory: 262264,
      peak_malloced_memory: 206880,
      does_zap_garbage: 0,
      number_of_native_contexts: 2,
      number_of_detached_contexts: 0,
      total_global_handles_size: 8192,
      used_global_handles_size: 2560,
      external_memory: 1737097
    }
    index.js
    14 Oct 10:49:27 - E:\Semestr5\Języki wysokiego poziomu w aplikacjach internetowych\WojciechMierzwa.github.io\uploads\images
    Server running at http://127.0.0.1:8888/ */

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
-/*const fs = require("fs");
-const data = fs.readFileSync('package.json');
-console.log("Początek skryptu.");
-console.log(data.toString());
-console.log("Koniec skryptu.");*/
-
-const fs = require("fs");
-console.log("Początek skryptu.");
-fs.readFile('package.json', function (err, data) {
-if (err) return console.error(err);
-console.log(data.toString());
+const http = require('http');
+const path = require("path");
+const util = require("util");
+const v8 = require("v8");
+const hostname = '127.0.0.1';
+const port = 8888;
+const server = http.createServer((request, response) => {
+response.statusCode = 200;
+response.setHeader('Content-Type', 'text/plain');
+response.end('Hello, from node.js serwer !\n');
 });
-console.log("Koniec skryptu.");
+server.listen(port, hostname, () => {
+util.log(v8.getHeapStatistics());
+console.log(path.basename(__filename));
+util.log(path.join(__dirname, 'uploads','images'));
+console.log(`Server running at http://${hostname}:${port}/`);
+});
 
-//WNIOSEK
+/*Po podmienieniu zawartości pliku dostaliśmy poniższy output. Wnioskiem jest, że nodemon jest przydatny gdy na bieżąco chcemy analizować zmiany 
+które zaszły po zmianie kodu bo nie trzeba ręcznie restartować servera, bo przy każdej zmianie automatycznie jest restartowany
 
-//1 Skrypt
-//Blokujące wywołania: Program czeka, aż zakończy się operacja, co może spowalniać inne zadania.
-
-//2 Skrypt
-//Nieblokujące wywołania: Program kontynuuje wykonywanie innych zadań bez czekania na zakończenie operacji, co zwiększa efektywność, zwłaszcza w operacjach I/O.
+Server running at http://127.0.0.1:8888/
+[nodemon] restarting due to changes...
+[nodemon] starting `node index.js`
+14 Oct 10:49:27 - {
+  total_heap_size: 4927488,
+  total_heap_size_executable: 262144,
+  total_physical_size: 4927488,      
+  total_available_size: 2194292408,  
+  used_heap_size: 4453104,
+  heap_size_limit: 2197815296,       
+  malloced_memory: 262264,
+  peak_malloced_memory: 206880,      
+  does_zap_garbage: 0,
+  number_of_native_contexts: 2,      
+  number_of_detached_contexts: 0,    
+  total_global_handles_size: 8192,   
+  used_global_handles_size: 2560,    
+  external_memory: 1737097
+}
+index.js
+14 Oct 10:49:27 - E:\Semestr5\Języki wysokiego poziomu w aplikacjach internetowych\WojciechMierzwa.github.io\uploads\images
+Server running at http://127.0.0.1:8888/ */
