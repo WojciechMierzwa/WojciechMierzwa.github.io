9d6adaeba55ceab9c712bc0589720c06c17e3687
WojciechMierzwa <wojciech.piotr.mierzwa@gmail.com>
2024-10-14 10:43:59 +0200

    Lab3Zad2
    //WNIOSEK
    
    //1 Skrypt
    //Blokujące wywołania: Program czeka, aż zakończy się operacja, co może spowalniać inne zadania.
    
    //2 Skrypt
    //Nieblokujące wywołania: Program kontynuuje wykonywanie innych zadań bez czekania na zakończenie operacji, co zwiększa efektywność, zwłaszcza w operacjach I/O.

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const wait = 3000;
-console.log("Początek skryptu...");
-setTimeout(function(){
-console.log("Upłynęły minimum 3 sek...")
-}, wait);
-console.log("koniec skryptu...");
+/*const fs = require("fs");
+const data = fs.readFileSync('package.json');
+console.log("Początek skryptu.");
+console.log(data.toString());
+console.log("Koniec skryptu.");*/
+
+const fs = require("fs");
+console.log("Początek skryptu.");
+fs.readFile('package.json', function (err, data) {
+if (err) return console.error(err);
+console.log(data.toString());
+});
+console.log("Koniec skryptu.");
 
 //WNIOSEK
 
-/* Node.js działa asynchronicznie czyli nie czeka na
-zakończenie długotrwałych operacji*/
+//1 Skrypt
+//Blokujące wywołania: Program czeka, aż zakończy się operacja, co może spowalniać inne zadania.
+
+//2 Skrypt
+//Nieblokujące wywołania: Program kontynuuje wykonywanie innych zadań bez czekania na zakończenie operacji, co zwiększa efektywność, zwłaszcza w operacjach I/O.
