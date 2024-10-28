0254d03f38d8bfec200e664eef4b863280d702dc
WojciechMierzwa <96776516+WojciechMierzwa@users.noreply.github.com>
2024-10-28 10:47:36 +0100

    Laboratorium 5 Zadanie 2
    ZADANIE 2. Obliczenia matematyczne.

--- node-projects/WojciechMierzwaNodeLab5/module_test.js
+++ node-projects/WojciechMierzwaNodeLab5/module_test.js
@@ -1,4 +1,4 @@
-const currency1 = require('./currency1');
+/*const currency1 = require('./currency1');
 
 const Currency = require('./currency2');
 const exchangeRates = {
@@ -36,5 +36,58 @@ testAmounts.forEach(amount => {
         } catch (error) {
             console.error(error.message);
         }
+    });
+});*/
+
+//zad2
+// user_interface.js
+
+const readline = require('readline');
+const math1 = require('./myMathModule1'); 
+const MyMathModule = require('./myMathModule2');  
+const math2 = new MyMathModule(); 
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function wykonajObliczenie(modul, operacja, a, b) {
+    switch (operacja) {
+        case '1':
+            return modul.add(a, b);
+        case '2':
+            return modul.subtract(a, b);
+        case '3':
+            return modul.multiply(a, b);
+        case '4':
+            return modul.divide(a, b);
+        case '5':
+            return modul.power(a, b);
+        default:
+            throw new Error("Nieznana operacja");
+    }
+}
+
+rl.question('Wybierz moduł (1 dla myMathModule1 (export), 2 dla myMathModule2 (class )): ', (wyborModulu) => {
+    const wybranyModul = wyborModulu === '1' ? math1 : math2;
+    const nazwaModulu = wyborModulu === '1' ? 'myMathModule1' : 'myMathModule2';
+
+    rl.question('Podaj pierwszą liczbę: ', (pierwszaLiczba) => {
+        const a = parseFloat(pierwszaLiczba);
+        rl.question('Podaj drugą liczbę: ', (drugaLiczba) => {
+            const b = parseFloat(drugaLiczba);
+
+            rl.question('Wybierz operację (dodaj (1), odejmij(2), pomnóż(3), podziel(4), potęga(5)): ', (operacja) => {
+                try {
+                    const wynik = wykonajObliczenie(wybranyModul, operacja, a, b);
+                    console.log(`\nWynik operacji ${operacja} (${a}, ${b}) za pomocą ${nazwaModulu} = ${wynik}`);
+                } catch (error) {
+                    console.error(error.message);
+                } finally {
+                    rl.close();
+                }
+            });
+        });
     });
 });
---
+++ node-projects/WojciechMierzwaNodeLab5/myMathModule1.js
@@ -0,0 +1,20 @@
+exports.add = function (a, b) {
+    return a + b;
+};
+
+exports.subtract = function (a, b) {
+    return a - b;
+};
+
+exports.multiply = function (a, b) {
+    return a * b;
+};
+
+exports.divide = function (a, b) {
+    if (b === 0) throw new Error("Nie można dzielić przez 0");
+    return a / b;
+};
+
+exports.power = function (a, b) {
+    return Math.pow(a, b);
+};
