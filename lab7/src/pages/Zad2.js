8f3d150f8927543f844a07290ae52d0fbb2f1e53
WojciechMierzwa <wojciech.piotr.mierzwa@gmail.com>
2024-11-12 12:47:15 +0100

    Laboratorium 7 Zadanie 1
    Zbuduj własny komponent funkcyjny wielokrotnego użytku i przekaż do niego dane
    poprzez „props”. Forma dowolna.

---
+++ lab7/src/pages/Zad2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Zad2() {
+  return (
+    <div>
+      <h1>Zadanie 2</h1>
+      <p>Treść strony dla Zadania 2.</p>
+    </div>
+  );
+}
+
+export default Zad2;
